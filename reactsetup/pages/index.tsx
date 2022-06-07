20b08de0ee4ebd0dfcd9b0b9af248d1aa2662c36
lirasusejdev <lis.amaral@grupoboticario.com.br>
2022-06-07 12:33:23 -0300

    add files

---
+++ reactsetup/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main>
+        <h1>Onboarding Flora</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
